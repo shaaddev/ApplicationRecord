67964c374bb92006ce678d6f84f531150a18782e
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-07-27 05:12:51 -0400

    feat: new favicon x rounded navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ export async function Navbar() {
   const { data: { user } } = await supabase.auth.getUser()
 
   return(
-    <nav className="sticky flex flex-col max-w-full h-max z-10 top-0 inset-x-o px-10 py-5 border-none bg-pink-800/100 text-slate-100  dark:bg-slate-800/50 backdrop-blur-xl  shadow-md ">
+    <nav className="sticky flex flex-col max-w-full h-max z-10 top-0 inset-x-o px-10 py-5 m-2 rounded-2xl border-none bg-pink-800/100 text-slate-100  dark:bg-slate-800/50 backdrop-blur-xl  shadow-md ">
       <div className="flex flex-row items-center justify-between px-5">
         <SpreadSheet className='w-6 h-6' />
         <ul className='hidden lg:flex flex-row gap-6 justify-end'>
