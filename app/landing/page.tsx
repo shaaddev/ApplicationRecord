12a65bdfead1e0fe92ac31fa37204f4bb00f59b9
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-08-16 13:15:47 -0400

    Added a landing page

---
+++ app/landing/page.tsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    <iframe
+      src="/landing/index.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    ></iframe>
+  );
+}
