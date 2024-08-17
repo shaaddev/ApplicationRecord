cba42c9692cdaa0c63793a1a13ed8f667c7282a5
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2024-08-17 09:06:23 -0400

    Changed the hover color based on status

--- components/Table/table-list.tsx
+++ components/Table/table-list.tsx
@@ -1,15 +1,34 @@
 import { JobProps } from "@/lib/info";
 import { TableRow, TableCell } from "@/components/ui/table";
 import { Detail } from "../table-details/detail"
-import Link from "next/link";
 
 export function TableList({ data }: { data: JobProps[] }) {
+  const getStatusColor = (status: string) => {
+    switch (status) {
+      case "Not Applied":
+        return "hover:text-gray-800"; 
+      case "Applied":
+        return "hover:text-yellow-400"; 
+      case "Phone Screen":
+        return "hover:text-orange-500";
+      case "Offer":
+        return "hover:text-green-600"; 
+      case "Hired":
+        return "hover:text-lime-600"; 
+      case "Rejected":
+        return "hover:text-red-600"; 
+      case "Ghosted":
+        return "hover:text-gray-500"; 
+      default:
+        return "hover:text-gray-500"; 
+    }
+  };
 
   return(
     <>
       {data.map((m, index) => (
         <Detail data={m} key={index}>
-          <TableRow className="hover:text-lime-600 hover:cursor-pointer">
+          <TableRow className={`hover:cursor-pointer ${getStatusColor(m.status)}`}>
             <TableCell className="font-medium p-5">{m.role}</TableCell>
             <TableCell>{m.company_name}</TableCell>
             <TableCell>{m.location}</TableCell>
