cd765d230354acf10d34a223ab939eb728d18444
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-09-02 14:29:41 -0400

    fix: middleware x auth route

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -9,18 +9,21 @@ export function createClient() {
     process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll()
+        get(name: string) {
+          return cookieStore.get(name)?.value
         },
-        setAll(cookiesToSet) {
+        set(name: string, value: string, options: CookieOptions) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            console.error('Error: ', error)
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {
+            console.error('Error: ', error)
           }
         },
       },
