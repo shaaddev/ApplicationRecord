10705fab2d7d56bb5297a62be262f6855f66694b
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-09-02 14:20:26 -0400

    fix: app/layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
