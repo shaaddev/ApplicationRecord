75733f2ede3a0ae055ce539ee79b959c5e653d35
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-09-06 14:33:42 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function LandingPage() {
   return (
-    <iframe
-      src="/landing/index.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-    ></iframe>
+    // <iframe
+    //   src="/landing/index.html"
+    //   style={{ width: "100%", height: "100vh", border: "none" }}
+    // ></iframe>
+    <main className="flex flex-col items-center justify-between p-10 lg:p-16">
+      <h1>Hello!</h1>
+      <p>Feel free to login below</p>
+      <LoginLink postLoginRedirectURL="/application-record">
+        Login
+      </LoginLink>
+    </main>
   );
 }
