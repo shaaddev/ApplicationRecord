ab8936b894a54b1e2515cdf8d18f21de96a92c86
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-10-05 14:36:15 -0400

    rollback

--- config/index.ts
+++ config/index.ts
@@ -2,5 +2,5 @@ export const CURRENCY = "usd";
 // Set your amount limits: Use float for decimal currencies and
 // Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
 export const MIN_AMOUNT = 10.0;
-export const MAX_AMOUNT = 100.0;
+export const MAX_AMOUNT = 500.0;
 export const AMOUNT_STEP = 5.0;
